7cd90e7ba25ae4f1523c90606f904383fd431ad6
Ethan Chen <intervigil@gmail.com>
2017-09-17 13:14:00 +0800

    lavender: Add basic USB HAL that reports no status change
    Change-Id: I6d130d324753c03ac514c4500c5764bfa46941dc
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- device.mk
+++ device.mk
@@ -443,7 +443,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.xiaomi_lavender
 
 # Vibrator
 PRODUCT_PACKAGES += \
