8b43662554cbb1168f3d0d720ffad3d2bb77d516
dianlujitao <dianlujitao@lineageos.org>
2018-02-20 22:47:08 +0800

    lavender: Build LiveDisplay HAL
    Change-Id: I1a1f630bd8c4670b3fe9c16d2493019f0aa56a95
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- device.mk
+++ device.mk
@@ -241,6 +241,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_lavender
 
+# Lineage Stuff
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-sdm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
