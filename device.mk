712ca3733739911675a37d1e6c7f3cfe96cd224d
Genkzsz11 <Genkzsz11@gmail.com>
2020-02-23 23:19:20 +0700

    DU

--- device.mk
+++ device.mk
@@ -224,7 +224,7 @@ PRODUCT_PACKAGES += \
 
 # HW crypto
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+    vendor.qti.hardware.cryptfshw@1.0-service-qti
 
 # Input
 PRODUCT_COPY_FILES += \
