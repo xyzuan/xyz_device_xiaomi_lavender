f4607c8e5d1b5445333d27528992d75f5ef95a7c
Genkzsz11 <Genkzsz11@gmail.com>
2020-02-24 00:07:43 +0700

    lavender: bring up DU

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,7 +5,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/du_lavender.mk
+    $(LOCAL_DIR)/du.mk
 
 COMMON_LUNCH_CHOICES := \
     du_lavender-user \
--- du.mk
+++ du.mk
@@ -12,10 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from lavender device
 $(call inherit-product, device/xiaomi/lavender/device.mk)
 
-# Inherit dalvik from native
-$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
-
-# Inherit some common DureX stuff.
+# Inherit some common DirtyUnicorns stuff.
 $(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
@@ -31,7 +28,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="lavender-user 9 PKQ1.180904.001 V10.3.9.0.PFGMIXM release-keys" \
 
 BUILD_FINGERPRINT := "google/crosshatch/crosshatch:10/QQ1A.200205.002/6084386:user/release-keys"
-
-DU_BUILD_TYPE := OFFICIAL
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
