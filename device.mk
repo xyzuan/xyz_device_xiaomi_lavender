12cf23c6252b543581b3bbdb1fd1ebaa944802dd
Genkzsz11 <Genkzsz11@gmail.com>
2020-03-30 14:30:51 +0700

    lavender: remove init.panel_info.sh *Useless for lavender

--- device.mk
+++ device.mk
@@ -355,7 +355,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     init.class_main.sh \
-    init.panel_info.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.usb.sh \
